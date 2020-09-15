fd00f83ccc6f49e049b73ae3481db014384cd9bd
IceDjuro <milandjurovic625@gmail.com>
2020-09-15 02:47:06 -0700

    Initial commit

---
+++ client/script.js
@@ -0,0 +1,7 @@
+let cm; // Global codemirror variable
+let changeMode;
+let currentFileOpened;
+feather.replace();
+require("./titlebar");
+require("./editor");
+require("./ui");
---
+++ index.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow, Menu } = require("electron");
+const { autoUpdater } = require("electron-updater");
+
+let dev = false;
+const args = process.argv.slice(2);
+args.forEach((val, index) => {
+
+    switch (val) {
+        case "--dev":
+            dev = true;
+            break;
+        default:
+            console.log("Invalid argument (" + index + "): " + val);
+            process.exit();
+    }
+});
+
+if (!dev) Menu.setApplicationMenu(null);
+
+function boot() {
+    autoUpdater.checkForUpdatesAndNotify();
+    let window = new BrowserWindow({
+        frame: false,
+        resizable: true,
+        icon: "assets/logo128.png",
+        minHeight: 400,
+        minWidth: 500,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        }
+    });
+    window.loadURL(`file://${__dirname}/client/index.html`);
+
+    window.on("closed", () => {
+        window = null;
+    });
+}
+
+app.on("ready", boot);
